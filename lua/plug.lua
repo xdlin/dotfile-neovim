dcb6bac17baaa4ef83fe55f321febd93d3115ded
Xiangdong Lin <linxiangdong@bytedance.com>
2023-03-09 09:55:10 +0800

    add 'cmp-cmdline' explicitly to fix nvim-cmp <Tab> not working issue
    * https://github.com/hrsh7th/nvim-cmp/issues/1468
    
    Signed-off-by: Xiangdong Lin <linxiangdong@bytedance.com>

--- lua/plug.lua
+++ lua/plug.lua
@@ -40,7 +40,10 @@ return require('packer').startup(function()
 
     -- auto completion
     use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-path'
     use 'saadparwaiz1/cmp_luasnip'
     use 'L3MON4D3/LuaSnip'
 
